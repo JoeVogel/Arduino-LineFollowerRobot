4ee67d7c7ca460f817612dd3dc084584b76df936
Joe Vogel <joe_vogel@live.com>
2014-09-15 16:36:44 -0300

    Update LineFollowerRobot.ino

--- LineFollowerRobot.ino
+++ LineFollowerRobot.ino
@@ -1,12 +1,19 @@
+/*
+	PROJETO SEGUIDOR DE LINHA
+	Acadêmicos: Fábio M. K. Lenzi, Joe J. Vogel
+	Orientador: Manfred H. Junior
+	Centro Universitário Católica de Santa Catarina
+*/
+
+//Declaração das variáveis utilizadas durante a execução do programa
 float Kp=25,Ki=0.6,Kd=2;
 float error=0, P=0, I=0, D=0, PID_value=0;
 float previous_error=0, previous_I=0;
-int sensor[5]={
-  0, 0, 0, 0, 0};
-int initial_motor_speed=80; /*Minimo 32*/
+int sensor[5]={0, 0, 0, 0, 0};
+int initial_motor_speed=80;
 
-const int motorA = 2; //Direita
-const int motorB = 4; //Esquerda
+const int motorA = 2; //Motor Direita
+const int motorB = 4; //Motor Esquerda
 const int motorA_PWM = 3;
 const int motorB_PWM = 5;
 int line;
@@ -19,10 +26,12 @@ const int ss4 = 11; // sensor 4
 const int ss5 = 12; // sensor 5   
 //const int ss6 = 13; // sensor Near
 
+//Declaração das funções
 void read_sensor_values(void);
 void calculate_pid(void);
 void motor_control(void);
 
+//Função de Setup do Arduino
 void setup()
 {
   pinMode(motorA,OUTPUT);  
@@ -37,11 +46,10 @@ void setup()
   pinMode(ss5, INPUT);
   //pinMode(ss6, INPUT);
 
-  //Serial.begin(9600);         //Enable Serial Communications
-
   delay(500);
 }
 
+//Função de execução continua do Arduino
 void loop()
 {
     read_sensor_values();
@@ -49,6 +57,7 @@ void loop()
     motor_control();
 }
 
+//Função responsável por fazer a leitura dos sensores do Robô
 void read_sensor_values()
 {
   line = 1;
@@ -64,7 +73,6 @@ void read_sensor_values()
     error=4;
   else if((sensor[0]==line)&&(sensor[1]==outline)&&(sensor[2]==outline)&&(sensor[4]==outline)&&(sensor[4]==outline))
     error=-4;
-
   
   /*if((sensor[0]==outline)&&(sensor[1]==outline)&&(sensor[2]==outline)&&(sensor[3]==line)&&(sensor[4]==line))
     error=3;
@@ -85,6 +93,7 @@ void read_sensor_values()
     error=0;
 }
 
+//Função responsável por fazer o cáculo PID
 void calculate_pid()
 {
 
@@ -98,34 +107,33 @@ void calculate_pid()
   previous_error=error;
 }
 
+//Função responsável por controlar a velocidade de cada motor
 void motor_control()
 {
   int left_motor_speed;
   int right_motor_speed;
 
-  // Calculating the effective motor speed:
+  // Calculo da velocidade de cada motor:
   left_motor_speed = initial_motor_speed+PID_value;
-  right_motor_speed = (initial_motor_speed-PID_value); //9 é a correção da variação entre motores    
+  right_motor_speed = initial_motor_speed-PID_value;  
 
   if(left_motor_speed < 0)
     left_motor_speed = 0;
   if(right_motor_speed < 0)
     right_motor_speed = 0;
 
-  analogWrite(motorA_PWM,left_motor_speed);   //Left Motor Speed
-  analogWrite(motorB_PWM,right_motor_speed * 1.1);  //Right Motor Speed
+  analogWrite(motorA_PWM,left_motor_speed);
+  analogWrite(motorB_PWM,right_motor_speed * 1.1);//*1.1 devido a diferença entre motores
 
-  /*Aciona Motores(Frente)*/
   digitalWrite(motorA,LOW);
   digitalWrite(motorB,LOW);
 }
 
+//Função responsável por parar o Robô
 void stopMotors(){
   analogWrite(motorA_PWM,0);   
   analogWrite(motorB_PWM,0);  
 }
-
-
 
 
 
